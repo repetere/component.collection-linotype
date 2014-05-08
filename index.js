2ae0dc0e14af6d2e3ac5e25ef9227ed8b07fd950
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-05-08 00:47:34 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * linotype
+ * http://github.amexpub.com/modules/linotype
+ *
+ * Copyright (c) 2013 AmexPub. All rights reserved.
+ */
+
+module.exports = require('./lib/linotype');
---
+++ test/linotype_test.js
@@ -0,0 +1,20 @@
+/*
+ * linotype
+ * https://github.com/condenast
+ *
+ * Copyright (c) 2013 Condé Nast. All rights reserved.
+ */
+
+'use strict';
+
+var should = require('chai').should();
+var linotype = require('../lib/linotype');
+
+describe('linotype', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      should.equal(-1, [1,2,3].indexOf(5));
+      should.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
